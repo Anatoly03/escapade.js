ae4df1a9020f47c625982d58f58da0d869627414
Anatoly Weinstein <anatoly.wein@gmail.com>
2024-06-05 09:58:38 +0200

    fix constants

--- src/data/consts.ts
+++ src/data/consts.ts
@@ -1,6 +1,6 @@
 
 export const ESCAPADE_URL = 'https://escapade.fun'
 
-export const ESCAPADE_API = 'https://escapade.fun:8080/api'
+export const ESCAPADE_API = 'https://escapade.fun:2053/api'
 
-export const SOCKET_URL = 'wss://escapade.fun:8080/ws'
+export const SOCKET_URL = 'wss://escapade.fun:2053/ws'
